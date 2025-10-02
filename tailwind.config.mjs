8a8b7da3ef8efe4b8f384d0bd0cbfed51bd97a68
neochocapic <juan.santibanezce@gmail.com>
2025-10-02 18:38:33 -0300

    subida plantilla

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+// tailwind.config.mjs
+export default {
+  theme: {
+    extend: {
+      colors: {
+        'gold': '#C79F3F',
+        'gold-dark': '#A88536',
+        'gray-light': '#4A4A4A',
+        'gray-dark': '#E5E5E5',
+ 
+      },
+    },
+  },
+
+}
